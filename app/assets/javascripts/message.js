e49ffa72ac80fde40e49eb575380cbf8aa2fd0c2
ma-sa2 <miyapon2ks@gmail.com>
2020-01-13 20:41:08 +0900

    generated files by scaffold

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,46 @@
-$(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
-  }
+$(function(){
+
+  var buildHTML = function(message) {
+    var html_common = `<div class="message" data-message-id=` + message.id + `>` +
+                      `<div class="upper-message">` +
+                        `<div class="upper-message__user-name">` +
+                          message.user_name +
+                        `</div>` +
+                        `<div class="upper-message__date">` +
+                          message.created_at +
+                        `</div>` +
+                      `</div>`
+
+    var html_content = `<div class="lower-message">` +
+                        `<p class="lower-message__content">` +
+                          message.content +
+                        `</p>`
+
+    var html_image = `<img src="` + message.image + `" class="lower-message__image" >`
+
+    if (message.content && message.image) {
+      //data-idが反映されるようにしている
+      var html = html_common +
+                 html_content +
+                 html_image +
+                    `</div>` +
+                  `</div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = html_common +
+                 html_content +
+                    `</div>` +
+                  `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = html_common +
+                    `<div class="lower-message">` +
+                 html_image +
+                    `</div>` +
+                  `</div>`
+    };
+    return html;
+  };
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -62,4 +64,31 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
